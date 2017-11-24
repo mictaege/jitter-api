2f92ed95dc051d7b1f87c61488f7e8450e8875d4
Michael Täge <mictaege@googlemail.com>
2017-11-24 23:15:02 +0100

    #9 It should be possible to add tags to the jitter annotations

---
+++ src/main/java/com/github/mictaege/jitter/api/Tags.java
@@ -0,0 +1,21 @@
+package com.github.mictaege.jitter.api;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.TYPE;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({PACKAGE, TYPE, FIELD, CONSTRUCTOR, METHOD})
+public @interface Tags {
+
+    String[] value();
+
+}
