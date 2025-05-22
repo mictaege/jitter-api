89d5b002e8a48694baf9c5bd0438c43dc99fef94
Michael Taege <mictaege@googlemail.com>
2025-05-22 22:19:09 +0200

    Publish to Central Portal using JReleaser

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
+import org.jreleaser.model.Active
+import org.jreleaser.model.Signing
+
 plugins {
     `java-library`
     `maven-publish`
     signing
+    id("org.jreleaser") version "1.18.0"
 }
 
 group = "io.github.mictaege"
-version = "2025.2"
+version = "2025.3-rc1"
 
 tasks.wrapper {
     gradleVersion = "8.7"
@@ -71,17 +75,41 @@ publishing {
     }
     repositories {
         maven {
-            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-            credentials {
-                username = if (hasProperty("ossrhUsername")) property("ossrhUsername") as String else ""
-                password = if (hasProperty("ossrhPassword")) property("ossrhPassword") as String else ""
-            }
+            url = uri(layout.buildDirectory.dir("staging-deploy").get().asFile.toURI())
         }
     }
 }
 
-signing {
-    sign(publishing.publications["mavenJava"])
+jreleaser {
+    project {
+        copyright.set("Michael Taege")
+        description.set("This repository contains the _jitter_ API. For using _jitter_ see the https://github.com/mictaege/jitter_plugin documentation.")
+    }
+    signing {
+        active.set(Active.ALWAYS)
+        armored.set(true)
+        checksums.set(true)
+        mode.set(Signing.Mode.FILE)
+        passphrase.set(if (hasProperty("centralPortalKeyPwd")) property("centralPortalKeyPwd") as String else "")
+        publicKey.set(if (hasProperty("centralPortalPublicKey")) property("centralPortalPublicKey") as String else "")
+        secretKey.set(if (hasProperty("centralPortalSecretKey")) property("centralPortalSecretKey") as String else "")
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    active.set(Active.ALWAYS)
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    username.set(if (hasProperty("centralPortalUsr")) property("centralPortalUsr") as String else "")
+                    password.set(if (hasProperty("centralPortalPwd")) property("centralPortalPwd") as String else "")
+                    stagingRepository(layout.buildDirectory.dir("staging-deploy").get().asFile.path)
+                }
+            }
+        }
+    }
+    release {
+        github {
+            enabled.set(false)
+        }
+    }
 }
